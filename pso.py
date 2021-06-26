a61c39c2645ce387fd7f4d448e3fa57246f721dc
nTaco <67748744+ntacoffee@users.noreply.github.com>
2021-06-26 16:58:57 +0900

    add comments

--- pso.py
+++ pso.py
@@ -17,38 +17,44 @@ c_2 = 1.49445
 
 
 def pso():
+
+    # initialize x and v with random variable
     x = (x_ub - x_lb) * np.random.rand(n_particles, test_func.dim) + x_lb
+    v = (x_ub - x_lb) * np.random.rand(n_particles, test_func.dim) + x_lb
+
+    # initialize inidivisual best
     x_hat = np.zeros((n_particles, test_func.dim))
     value_x_hat = np.inf * np.ones((n_particles,))
+
+    # initialize global best
     x_hat_g = np.zeros((test_func.dim,))
     value_x_hat_g = np.inf
-    v = (x_ub - x_lb) * np.random.rand(n_particles, test_func.dim) + x_lb
 
     for i_step in range(max_steps):
 
         # evaluate all perticles
         value = test_func(x)
 
-        # update x_hat
+        # update indivisual best
         indices_better_x = np.where(value < value_x_hat)
         x_hat[indices_better_x] = x[indices_better_x]
         value_x_hat[indices_better_x] = value[indices_better_x]
 
-        # update x_hat_g
+        # update global best
         idx_best_x = np.argmin(value)
         if value[idx_best_x] < value_x_hat_g:
             x_hat_g = x[idx_best_x]
             value_x_hat_g = value[idx_best_x]
-
-        # generate random value
-        r_1 = np.random.rand(n_particles, 1)
-        r_2 = np.random.rand(n_particles, 1)
 
         print(
             "Iteration {:>5d}: Current Best is {:.5f} at ({:.3f}, {:.3f})".format(
                 i_step + 1, value_x_hat_g, x_hat_g[0], x_hat_g[1]
             )
         )
+
+        # generate random value for update
+        r_1 = np.random.rand(n_particles, 1)
+        r_2 = np.random.rand(n_particles, 1)
 
         # compute next x, v
         x_next = np.clip(x + v, x_lb, x_ub)
